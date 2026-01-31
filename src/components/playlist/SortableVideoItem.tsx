325adce121306e24cebd7d19fc4d23461742c203
[dyad] <marcelo.net77@hotmail.com>
2026-01-31 10:09:48 +0000

    refactor: Applying dry principles

--- src/components/playlist/SortableVideoItem.tsx
+++ src/components/playlist/SortableVideoItem.tsx
@@ -1,4 +1,4 @@
-import { useSortable } from '@dnd-kit/sortable';
+import { useSortable } from '@dnd-kit/core';
 import { CSS } from '@dnd-kit/utilities';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
@@ -43,8 +43,6 @@ export function SortableVideoItem({
     transform: CSS.Transform.toString(transform),
     transition,
   };
-
-  // Removed local formatDuration function, now using the imported one
 
   return (
     <div
